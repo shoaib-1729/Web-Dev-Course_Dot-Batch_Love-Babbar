59782d2e3e802029ddbbce376e671202194acd13
shoaib-1729 <shoaibahktar56@gmail.com>
2023-10-19 17:35:39 +0530

    Commit

---
+++ 07_SASS-CSS/myApp/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles/app.scss";
+
+function App() {
+
+  return (
+    <>
+
+     <div className="product-card">
+          <h1>Sample</h1>
+          <a href="">Sample Link</a>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
